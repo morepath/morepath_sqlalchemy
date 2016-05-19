8f6a3c4f77d428ee61e6552ed37723a9707e51ef
Henri Hulski <henri.hulski@gazeta.pl>
2016-05-19 23:04:12 +0200

    Fix url

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author='Morepath developers',
     author_email='morepath@googlegroups.com',
     license="BSD",
-    url="https://github.com/morepath/morepath_batching",
+    url="https://github.com/morepath/morepath_sqlalchemy",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
