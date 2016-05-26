76ea76129bfb94c07cd7c52e8c2491e06d851ff9
Henri Hulski <henri.hulski@gazeta.pl>
2016-05-26 18:10:42 +0200

    Not test parameters for add and not clear database

--- morepath_sqlalchemy/tests/test_sqlalchemy.py
+++ morepath_sqlalchemy/tests/test_sqlalchemy.py
@@ -12,7 +12,6 @@ from morepath_sqlalchemy.model import Base
 def setup_module(module):
     engine = sqlalchemy.create_engine('sqlite:///:memory:')
     Session.configure(bind=engine)
-    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
     morepath.disable_implicit()
@@ -26,9 +25,7 @@ def test_documents():
     collection_response = c.get('/documents')
 
     assert collection_response.json["documents"] == []
-    assert "http://localhost/documents/add?" in collection_response.json["add"]
-    assert "limit=10" in collection_response.json["add"]
-    assert "offset=0" in collection_response.json["add"]
+    assert "http://localhost/documents/add" in collection_response.json["add"]
     assert collection_response.json["previous"] is None
     assert collection_response.json["next"] is None
 
@@ -72,11 +69,9 @@ def test_previous_next():
     }]
 
     assert response.json["documents"] == expected_documents
-    assert "http://localhost/documents/add?" in response.json["add"]
-    assert "limit=1" in response.json["add"]
-    assert "offset=0" in response.json["add"]
+    assert "http://localhost/documents/add" in response.json["add"]
     assert response.json["previous"] is None
-    assert "http://localhost/documents?" in response.json["next"]
+    assert "http://localhost/documents" in response.json["next"]
     assert "limit=1" in response.json["next"]
     assert "offset=1" in response.json["next"]
 
@@ -89,10 +84,8 @@ def test_previous_next():
     }]
 
     assert response.json["documents"] == expected_documents
-    assert "http://localhost/documents/add?" in response.json["add"]
-    assert "limit=1" in response.json["add"]
-    assert "offset=1" in response.json["add"]
-    assert "http://localhost/documents?" in response.json["previous"]
+    assert "http://localhost/documents/add" in response.json["add"]
+    assert "http://localhost/documents" in response.json["previous"]
     assert "limit=1" in response.json["previous"]
     assert "offset=0" in response.json["previous"]
     assert response.json["next"] is None
