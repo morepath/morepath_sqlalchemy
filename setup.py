3354b885e49f4a6906816924f6a7474ba1831b00
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2020-10-01 02:53:32 +0530

    Updated supported Python versions (#5)
    * updated tox.ini with latest python versions
    
    * Updated setup.py and .travis.yml with latest python versions
    
    * Updated .travis.yml with new matrix format
    
    * Update CHANGES.rst

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ setup(
         'Environment :: Web Environment',
         'License :: OSI Approved :: BSD License',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
